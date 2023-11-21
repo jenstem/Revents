624038f0324eb95337a9c4e98c24e38081bff752
Jen <jennifer.stemkowski@gmail.com>
2023-11-21 17:51:11 -0500

    Add EventDashboard

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,11 +1,14 @@
 // import './styles.css'
 
+import EventDashboard from "../../features/events/dashboard/EventDashboard"
+
 function App() {
 
 
   return (
     <>
-    <h1>Welcome to Revents</h1>
+      <h1>Welcome to Revents</h1>
+      <EventDashboard />
     </>
   )
 }
