e34183dd1bc2e7141c4ffa3762ba7fa8e166c463
Jen <jennifer.stemkowski@gmail.com>
2023-11-30 10:11:04 -0500

    Add counter and Scratch

--- src/features/scratch/testSlice.ts
+++ src/features/scratch/testSlice.ts
@@ -11,5 +11,18 @@ const initialState: State = {
 export const testSlice = createSlice({
     name: 'test',
     initialState,
-    reducers: {}
+    reducers: {
+        increment: (state) => {
+            state.data += 1
+        },
+        decrement: (state) => {
+            state.data -= 1
+        },
+        // will update the state by the amount passed in the action ( + or - 1)
+        incrementByAmount: (state, action) => {
+            state.data += action.payload
+        },
+    }
 })
+// export the actions
+export const {incrementByAmount, increment, decrement} = testSlice.actions
