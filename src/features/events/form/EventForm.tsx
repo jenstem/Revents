cb37cde95cf53d28a3ecd61554df42f07029171c
Jen <jennifer.stemkowski@gmail.com>
2023-12-04 11:29:19 -0500

    Add required fields to EventForm.tsx

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../app/store/store";
 import { createEvent, updateEvent } from "../eventSlice";
 import { createId } from "@paralleldrive/cuid2";
+import { FieldValues, useForm } from "react-hook-form";
 
 
 // import { AppEvent } from "../../../app/types/event";
@@ -22,7 +23,7 @@ import { createId } from "@paralleldrive/cuid2";
 // add updateEvent to Props
 // remove props when using router
 // export default function EventForm({ setFormOpen, addEvent, selectedEvent, updateEvent }: Props) {
-    export default function EventForm() {
+export default function EventForm() {
     // add useParams hook
     // need to change const { id } = use Params() to let { id } = useParams()
     // we need to change the id to let because we'll be updating the id
@@ -30,44 +31,52 @@ import { createId } from "@paralleldrive/cuid2";
     const event = useAppSelector(state => state.events.events.find(e => e.id === id));
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
+    // this comes from react-hook-form
+    const { register, handleSubmit, formState: {errors, isValid, isSubmitting} } = useForm({
+        mode: 'onTouched'
+    });
 
     // create an object we can pass into our useState
     // ?? = if selected event is null use what's inside this const
     // do NOT need selectedEvent when using router
     // const initialValues = selectedEvent ?? {
-        // we replace this with event ?? meaning use event otherwise use the following
-        const initialValues = event ?? {
-        title: '',
-        category: '',
-        description: '',
-        city: '',
-        venue: '',
-        date: ''
-    }
+    // we replace this with event ?? meaning use event otherwise use the following
+    // with react-hook-form we don't need to use initialValues
+    // const initialValues = event ?? {
+    //     title: '',
+    //     category: '',
+    //     description: '',
+    //     city: '',
+    //     venue: '',
+    //     date: ''
+    // }
 
     // state
-    const [values, setValues] = useState(initialValues);
+    // with react-hook-form we don't need to use state
+    // const [values, setValues] = useState(initialValues);
 
 
     // what will happen when we submit our form
-    function onSubmit() {
+    function onSubmit(data: FieldValues) {
+        console.log(data);
         // remove any props you're no longer using when using router
         // check to see if there's anything inside selectedEvent
         // if we do have a selectedEvent then we want to update the event
-        id = id ?? createId();
-        event
-            ? dispatch(updateEvent({...event, ...values}))
-            : dispatch(createEvent({...values, id, hostedBy: 'bob', attendees: [], hostPhotoURL: ''}));
+        // id = id ?? createId();
+        // event
+        //     ? dispatch(updateEvent({ ...event, ...values }))
+        //     : dispatch(createEvent({ ...values, id, hostedBy: 'bob', attendees: [], hostPhotoURL: '' }));
         // we want to navigate to the event we just updated or created
-        navigate(`/events/${id}`);
+        // navigate(`/events/${id}`);
     }
 
-    function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
-        const { name, value } = e.target;
-        // we want to spread the values and then set the name to the value of just update one value
-        // ...values gives us all values and [name]: value specifies which value to update
-        setValues({ ...values, [name]: value })
-    }
+    // NO LONGER need this function, register with react-hook-form takes its place
+    // function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    //     const { name, value } = e.target;
+    //     // we want to spread the values and then set the name to the value of just update one value
+    //     // ...values gives us all values and [name]: value specifies which value to update
+    //     setValues({ ...values, [name]: value })
+    // }
 
     return (
         <Segment clearing>
@@ -75,58 +84,65 @@ import { createId } from "@paralleldrive/cuid2";
             {/* remove props you aren't using with router */}
             {/* <Header content={selectedEvent ? 'Update Event' : 'Create Event'} /> */}
             {/* replace selectedEvent with event because of useParam hook */}
-            <Header content={event ? 'Update event' : 'Create Event'} />
-            <Form onSubmit={onSubmit}>
-                <Form.Field>
-                    <input
-                        type='text'
-                        placeholder='Event Title'
-                        value={values.title}
-                        // we'll use the name and value to update the state
-                        name='title'
-                        // this is a change event for an HTML element
-                        onChange={e => handleInputChange(e)}
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <input type='text' placeholder='Category'
-                        value={values.category}
-                        name='category'
-                        onChange={e => handleInputChange(e)}
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <input type='text' placeholder='Description'
-                        value={values.description}
-                        name='description'
-                        onChange={e => handleInputChange(e)} />
-                </Form.Field>
-                <Form.Field>
-                    <input type='text' placeholder='City'
-                        value={values.city}
-                        name='city'
-                        onChange={e => handleInputChange(e)} />
-                </Form.Field>
-                <Form.Field>
-                    <input type='text' placeholder='Venue'
-                        value={values.venue}
-                        name='venue'
-                        onChange={e => handleInputChange(e)}
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <input type='date' placeholder='Date'
-                        value={values.date}
-                        name='date'
-                        onChange={e => handleInputChange(e)}
-                    />
-                </Form.Field>
-                <Button type='submit' floated='right' positive content='Submit' />
+            {/* sub = subheader */}
+            <Header content={'Event details'} sub color='teal'/>
+            {/* we want to use the react-hook-form onSubmit */}
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Input
+                    placeholder='Event Title'
+                    defaultValue={event?.title || ''}
+                    // use {required: true} for a required field
+                    {...register('title', {required: true})}
+                    // this will notify users that the title is required
+                    error={errors.title && 'Title is required'}
+                // we'll use the name and value to update the state
+                // name='title'
+                // this is a change event for an HTML element
+                // onChange={e => handleInputChange(e)}
+                />
+
+                <Form.Input
+                    placeholder='Category'
+                    defaultValue={event?.category || ''}
+                    {...register('category', {required: 'Category is required'})}
+                    error={errors.category && errors.category.message}
+                />
+                {/* Form.TextArea is a text area for a form */}
+                <Form.TextArea
+                    placeholder='Description'
+                    defaultValue={event?.description || ''}
+                    {...register('description', {required: 'Description is required'})}
+                    error={errors.description && errors.description.message}
+                />
+                <Header sub content='Location details' color='teal'/>
+                <Form.Input
+                    placeholder='City'
+                    defaultValue={event?.city || ''}
+                    {...register('city', {required: 'City is required'})}
+                    error={errors.city && errors.city.message}
+                />
+
+                <Form.Input
+                    placeholder='Venue'
+                    defaultValue={event?.venue || ''}
+                    {...register('venue', {required: 'Venue is required'})}
+                    error={errors.venue && errors.venue.message}
+                />
+
+                <Form.Input
+                    type='date'
+                    placeholder='Date'
+                    defaultValue={event?.date || ''}
+                    {...register('date', {required: 'Date is required'})}
+                    error={errors.date && errors.date.message}
+                />
+
+                <Button loading={isSubmitting} type='submit' disabled={!isValid} floated='right' positive content='Submit' />
                 {/* the onClick is set to false because we want to close the form
                     or rather have the form disappear when we click the cancel button */}
-                    {/* remove onClick when using router */}
+                {/* remove onClick when using router */}
                 {/* <Button onClick={() => setFormOpen(false)} type='button' floated='right' content='Cancel' /> */}
-                <Button as={Link} to='/events' type='button' floated='right' content='Cancel' />
+                <Button disabled={isSubmitting} as={Link} to='/events' type='button' floated='right' content='Cancel' />
             </Form>
         </Segment>
     )
