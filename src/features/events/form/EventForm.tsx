2dc2e00ff0c619db3e9732104fb932a514f8fb48
Jen <jennifer.stemkowski@gmail.com>
2023-12-07 15:14:20 -0500

    Add createEvent & updateEvent functions

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -1,12 +1,16 @@
 import { Button, Form, Header, Segment } from "semantic-ui-react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../../app/store/store";
-import { createEvent, updateEvent } from "../eventSlice";
-import { createId } from "@paralleldrive/cuid2";
+import { useAppSelector } from "../../../app/store/store";
+// import { createEvent, updateEvent } from "../eventSlice";
+// import { createId } from "@paralleldrive/cuid2";
 import { Controller, FieldValues, useForm } from "react-hook-form";
 import { categoryOptions } from "./categoryOptions";
 import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker from 'react-datepicker';
+import { AppEvent } from "../../../app/types/event";
+import { doc, setDoc, collection } from "firebase/firestore";
+import { db } from "../../../app/config/firebase";
+import { Timestamp, updateDoc } from "firebase/firestore";
 
 
 // import { AppEvent } from "../../../app/types/event";
@@ -29,10 +33,46 @@ export default function EventForm() {
     // add useParams hook
     // need to change const { id } = use Params() to let { id } = useParams()
     // we need to change the id to let because we'll be updating the id
-    let { id } = useParams();
+    // we can change it back to const because we're no longer updating the id
+    const { id } = useParams();
     const event = useAppSelector(state => state.events.events.find(e => e.id === id));
-    const dispatch = useAppDispatch();
+    // const dispatch = useAppDispatch();
     const navigate = useNavigate();
+
+    // update event function
+    async function updateEvent(data: AppEvent) {
+        if (!event) return;
+        // doc comes from firestore, db is database, collection you're going to update, id of event
+        const docRef = doc(db, 'events', event.id);
+        // updateDoc is from firestore
+        await updateDoc(docRef, {
+            // specify data you want to update, spread the data
+            ...data,
+            // we need to turn date into a firestore Timestamp
+            // you can't go from string to type Date, so we need to go from string to Date to Timestamp
+            // so we add in "as unknown" and then "as Date"
+            // must use "as unknown" otherwise it won't accept it
+            date: Timestamp.fromDate(data.date as unknown as Date)
+        })
+    }
+    // create event function
+    // we're passing FieldValues because we don't have an event to work with
+    async function createEvent(data: FieldValues) {
+        // we need firestore to create an id for us and we want to pass id to a navigate function
+        // so that we can redirect to the event we just created, we don't know the id yet
+        // therefore, we need to reference the document BEFORE it's been created
+        // can use doc and setDoc() to create a document with an auto-generated id
+        const newEventRef = doc(collection(db, 'events'));
+        await setDoc(newEventRef, {
+            ...data,
+            hostedBy: 'bob',
+            attendees: [],
+            hostPhotoURL: '',
+            date: Timestamp.fromDate(data.date as unknown as Date),
+        })
+        return newEventRef;
+    }
+
     // this comes from react-hook-form
     // we need control for the Controller and dropdown option for category
     const { register, handleSubmit, control, setValue, formState: { errors, isValid, isSubmitting } } = useForm({
@@ -60,21 +100,34 @@ export default function EventForm() {
 
 
     // what will happen when we submit our form
-    function onSubmit(data: FieldValues) {
+    async function onSubmit(data: FieldValues) {
+        try {
+            if (event) {
+                await updateEvent({...event, ...data});
+                navigate(`/events/${event.id}`);
+            } else {
+                const ref = await createEvent(data);
+                navigate(`/events/${ref.id}`);
+            }
+        } catch (error) {
+            console.log(error);
+        }
         // remove any props you're no longer using when using router
         // check to see if there's anything inside selectedEvent
         // if we do have a selectedEvent then we want to update the event
-        id = id ?? createId();
-        event
+        // remove this code because we created a function for updateEvent and createEvent above instead
+        // id = id ?? createId();
+        // event
         // need to add date: data.date.toString() to change the date from a date object to a string
-            ? dispatch(updateEvent({ ...event, ...data, date: data.date.toString() }))
-            : dispatch(createEvent({ ...data, id, hostedBy: 'bob', attendees: [], hostPhotoURL: '', date: data.date.toString() }));
-            // Above we changed ...values to ...data because we're using react-hook-form and we're no longer using values
-            // event
-            // ? dispatch(updateEvent({ ...event, ...values }))
-            // : dispatch(createEvent({ ...values, id, hostedBy: 'bob', attendees: [], hostPhotoURL: '' }));
+        // dispatching update to store and then we see that update in our browser
+        // ? dispatch(updateEvent({ ...event, ...data, date: data.date.toString() }))
+        // : dispatch(createEvent({ ...data, id, hostedBy: 'bob', attendees: [], hostPhotoURL: '', date: data.date.toString() }));
+        // Above we changed ...values to ...data because we're using react-hook-form and we're no longer using values
+        // event
+        // ? dispatch(updateEvent({ ...event, ...values }))
+        // : dispatch(createEvent({ ...values, id, hostedBy: 'bob', attendees: [], hostPhotoURL: '' }));
         // we want to navigate to the event we just updated or created
-        navigate(`/events/${id}`);
+        // navigate(`/events/${id}`);
     }
 
     // NO LONGER need this function, register with react-hook-form takes its place
@@ -131,7 +184,7 @@ export default function EventForm() {
                             // use onChange to update the state, so you don't get undefined for your category
                             // put a _ for the first parameter because we don't need it, we only need the second parameter
                             // we use the _ as a placeholder
-                            onChange={(_, d) => setValue('category', d.value, {shouldValidate: true})}
+                            onChange={(_, d) => setValue('category', d.value, { shouldValidate: true })}
                             error={errors.category && errors.category.message}
                         />
                     )}
@@ -159,22 +212,22 @@ export default function EventForm() {
                 />
                 {/* Use Form.Field so we get the styling */}
                 <Form.Field>
-                        <Controller
-                            name='date'
-                            control={control}
-                            rules={{ required: 'Date is required' }}
-                            defaultValue={event && new Date(event.date) || null}
-                            render={({ field }) => (
-                                <DatePicker
-                                    selected={field.value}
-                                    onChange={value => setValue('date', value, {shouldValidate: true})}
-                                    showTimeSelect
-                                    timeCaption="time"
-                                    dateFormat='MMM d, yyyy h:mm aa'
-                                    placeholderText="Event date and time"
-                                />
-                            )}
-                        />
+                    <Controller
+                        name='date'
+                        control={control}
+                        rules={{ required: 'Date is required' }}
+                        defaultValue={event && new Date(event.date) || null}
+                        render={({ field }) => (
+                            <DatePicker
+                                selected={field.value}
+                                onChange={value => setValue('date', value, { shouldValidate: true })}
+                                showTimeSelect
+                                timeCaption="time"
+                                dateFormat='MMM d, yyyy h:mm aa'
+                                placeholderText="Event date and time"
+                            />
+                        )}
+                    />
                 </Form.Field>
                 {/* <Form.Input
                     type='date'
